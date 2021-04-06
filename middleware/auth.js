6a2ce1246c6b86aeab2c92f40f8bc71348191819
Rahul Pawar <pawarrahul699@gmail.com>
2021-04-06 15:57:43 +0530

    chages

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,7 +10,6 @@ try{
 	const decode = jwt.verify(token, jwtPrivateKey);
     //console.log(decode);
 	req.userData = decode; //req.userData contains userDetails in this token
-    //var decodedValue = JSON.parse(window.atob(base64Url));
 	next();
 
 }catch(error){
